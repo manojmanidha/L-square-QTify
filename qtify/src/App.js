afcc725a486e080c6aaa4475f5d0993d7d4a14f6
Manoj KS <manojks.062001@gmail.com>
2024-01-27 22:52:34 +0530

    herosection

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar1/NavBar';
+import { Hero } from './components/Hero/Hero';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Hero/>
     </div>
   );
 }
